b9b593b3999ccfc1ca218b9e8b19745b4af6a56d
Soundarya <67475352+soundarya12s@users.noreply.github.com>
2023-08-30 16:03:10 +0530

    Update Operations.cs

--- ProjectReviewManagement/Operations.cs
+++ ProjectReviewManagement/Operations.cs
@@ -21,6 +21,14 @@ namespace ProjectReviewManagement
             var result = list.Where(x => x.Rating > 3 && (x.ProductID == 1 || x.ProductID == 4 || x.ProductID == 9));
             Display(result.ToList());
         }
+         public void UsingGroupBy(List<Product> list)
+        {
+            var result = list.GroupBy(x => x.ProductID).Select(x => new {ProductId = x.Key,Count = x.Count()});
+            foreach(var data  in result)
+            {
+                Console.WriteLine(data.ProductId + " " + data.Count);
+            }
+        }
         public void Display(List<Product> list)
         {
             foreach (var item in list)
